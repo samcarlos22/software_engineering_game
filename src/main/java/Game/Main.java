2ee2c9dc93902a99bcee6b922e8101a730ac0e7b
Samuel Carlos <samcarlos22@gmail.com>
2019-05-06 16:41:22 +0000

    Added start screen and Leaderboard class. Modified POM.

--- src/main/java/Game/Main.java
+++ src/main/java/Game/Main.java
@@ -3,11 +3,18 @@ package Game;
 import Controller.Controller;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -27,11 +34,50 @@ public class Main extends Application {
 
         //Set up view
         controller.createView();
-        controller.getView().setPrefSize(400, 400);
-        controller.getView().setCenter(controller.getBoard());
+        controller.getView().setPrefSize(600, 480);
+        //controller.getView().setCenter(controller.getBoard());
         controller.getView().setAlignment(controller.getOutput(), Pos.CENTER);
         controller.getView().setMargin(controller.getOutput(), new Insets(12,12,30,12));
-        controller.getView().setBottom(controller.getOutput());
+        //controller.getView().setBottom(controller.getOutput());
+
+
+        //Set up leaderboard
+        Label p1 = new Label("Player 1");
+        Label p2 = new Label("Player 2");
+        Label p3 = new Label("Player 3");
+        Label p4 = new Label("Player 4");
+        Label p5 = new Label("Player 5");
+        VBox leaderboard = new VBox();
+        leaderboard.getChildren().addAll(p1, p2, p3, p4, p5);
+        leaderboard.setAlignment(Pos.CENTER);
+        controller.getView().setAlignment(leaderboard, Pos.CENTER);
+        controller.getView().setMargin(leaderboard, new Insets(12,12,12,12));
+        //controller.getView().setRight(leaderboard);
+
+        //Set up start screen
+        VBox startScreen = new VBox();
+        TextField name = new TextField("Player Name");
+        name.setAlignment(Pos.CENTER);
+        name.setMaxSize(200, 200);
+        controller.getView().setAlignment(name, Pos.CENTER);
+        Button start = new Button("Start");
+        Button exit = new Button("Exit");
+        start.setOnAction(actionEvent -> {
+            start.setText("Reset");
+            startScreen.getChildren().remove(name);
+            controller.getView().setAlignment(start, Pos.CENTER);
+            controller.getView().setCenter(controller.getBoard());
+            controller.getView().setRight(leaderboard);
+            controller.getView().setLeft(startScreen);
+            controller.getView().setTop(new Label("Welcome to the game " + name.getText()));
+            controller.getView().setBottom(controller.getOutput());
+            leaderboard.getChildren().add(new Label(name.getText()));
+        });
+        exit.setOnAction(actionEvent -> Platform.exit());
+        startScreen.getChildren().addAll(name, start, exit);
+        startScreen.setAlignment(Pos.CENTER);
+        controller.getView().setAlignment(startScreen, Pos.CENTER);
+        controller.getView().setCenter(startScreen);
 
         //Set up scene
         controller.createScene(controller.getView());
